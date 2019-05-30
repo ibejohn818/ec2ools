bbfc4f3c1a430c6344ef613f4fd080d04c199d5e
John Hardy <john.hardy@me.com>
2019-05-30 13:13:19 -0700

    - init repository

---
+++ ec2ools/cli-eips.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+"""Console script for ec2ools."""
+import sys
+import click
+
+
+@click.command()
+def main(args=None):
+    pass
+
+if __name__ == "__main__":
+    sys.exit(main())  # pragma: no cover
